e76b627c1fa2efba7c2406aba66f9bfdf26043ef
Anthony_S <anthony.stannard@baesystems.com>
2020-06-16 23:54:40 +0100

    Updated Thesis, POD Parameterisation section is finished

--- IDWDeform.py
+++ IDWDeform.py
@@ -1,29 +1,30 @@
-### <Summary>
-### Calculates the displacements of the volume points
-### using the displacements of each surface point.
-### Currently doesn't enforce that farfield stays fixed.
+import time
+import numpy as np
 ### </Summary>
 ### <Returns>
 ### A vector of CFD displacements as a numpy array.
 ### </Returns>
-def IDWDeform(self):
+def IDWDeform(xyzVol, xyzSurf, dxyzVol, dxyzSurf):
 	np.seterr(divide="raise")
 	np.seterr(invalid="raise")
-	xyz = self.GetMeshUndeformedCoordinates()
-	dxyzSurf = self.GetDisplacements()
-	dxyzVol = np.zeros(xyz.shape)
+	# xyz = self.GetMeshUndeformedCoordinates()
+	# dxyzSurf = self.GetDisplacements()
+	# dxyzVol = np.zeros(xyz.shape)
+	weights = np.ones(xyzSurf.shape[0])
+	weights[25000] = 0
 	
-	for i in range(xyz.shape[0]):
+	for i in range(xyzVol.shape[0]):
 		try:
-			r = xyz[i] - self.basePoints
-			d = np.einsum('ij,ij->i', r, r)
-			weights = d*d # Equivalent of np.linalg(...) ** 4
-			weights = 1.0/weights
+			r = xyz[i] - xyzSurf
+			#d = np.einsum('ij,ij->i', r, r)
+			#weights = d*d # Equivalent of np.linalg(...) ** 4
+			#weights = 1.0/weights
+			weights = np.linalg.norm(r, axis=1) ** (-3.5)
 		except:
 			# Catch the div0 if the current volume point is
 			# also a surface point.
-			surfInd = np.where(0.0 == weights)[0]
-			dxyzVol[i] = dxyzSurf[surfInd]
+			#surfInd = np.where(0.0 == weights)[0]
+			#dxyzVol[i] = dxyzSurf[surfInd]
 			continue
 		
 		weightSum = np.sum(weights)
@@ -33,3 +34,18 @@ def IDWDeform(self):
 		dxyzVol[i,2] = np.dot(weights, dxyzSurf[:,2])
 	
 	return dxyzVol
+
+nvol = 6000000
+nsurf = 50000
+
+xyzVol = np.random.rand(nvol,3)
+xyzSurf = np.random.rand(nsurf,3)
+dxyzVol = np.zeros(xyzVol.shape)
+dxyzSurf = np.random.rand(nsurf,3)
+
+start = time.time()
+dxyzVol = IDWDeform(xyzVol, xyzSurf, dxyzVol, dxyzSurf)
+end = time.time()
+seconds = end - start
+print(f"Took {seconds} seconds")
+print(f"Size {dxyzVol.shape}")
